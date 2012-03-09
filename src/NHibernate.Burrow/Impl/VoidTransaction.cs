9b655256cdba22bd91373690a9fd04e2151196db
firefly4321 <firefly4321@gmail.com>
2012-03-09 03:35:18 -0600

    initial commit

---
+++ src/NHibernate.Burrow/Impl/VoidTransaction.cs
@@ -0,0 +1,45 @@
+using System;
+
+namespace NHibernate.Burrow.Impl
+{
+    /// <summary>
+    /// a transaction manager that does no transaction management
+    /// </summary>
+    /// <remarks>
+    /// this is for in manual transaction mode, so that client can control the transaction itself.
+    /// </remarks>
+    internal class VoidTransaction : ITransaction
+    {
+        #region ITransaction Members
+
+        public void Begin(ISession sess)
+        {
+            return;
+        }
+
+        public void Commit()
+        {
+            return;
+        }
+
+        public void Rollback()
+        {
+            return;
+        }
+
+        /// <summary>
+        /// whether a transaction has begun
+        /// </summary>
+        public bool InTransaction
+        {
+            get { return false; }
+        }
+
+        public void Dispose()
+        {
+            return;
+        }
+
+        #endregion
+    }
+}
